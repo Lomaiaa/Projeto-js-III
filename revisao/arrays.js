a087242508db27b91e38ead5564bf9ae6be1c9c3
Lomaiaa <lorena14ma@yahoo.com.br>
2022-08-08 16:34:39 -0300

    projeto-js-III

--- revisao/arrays.js
+++
@@ -1,6 +0,0 @@
-const tipos = ['String', 'Numeros', 'Booleanos']
-const cidades = new Array("Recife", "Campina Grande", "Salvador", "Palmas")
-const tecnologias = Array.of("HTML", "CSS", "JS");
-const alfabeto = "abcdefghijklmnopqrstuvwxyz".split('');
-
-console.log(tipos[2])
